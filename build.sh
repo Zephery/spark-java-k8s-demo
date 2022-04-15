d9ea030942d9362f7cae56eb1a6f6b45d5d2efbf
wenzhihuai <1570631036@qq.com>
2022-04-16 00:23:25 +0800

    update

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mvn clean -DskipTests=true assembly:assembly
 
-docker build . -t zepherywen/spark-java-k8s-demo:latest
+docker build . -t ccr.ccs.tencentyun.com/wzh-image/spark-java-k8s-demo:latest
 
-docker push zepherywen/spark-java-k8s-demo:latest
+docker push ccr.ccs.tencentyun.com/wzh-image/spark-java-k8s-demo:latest
