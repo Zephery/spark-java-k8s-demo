cfb6e1b498341383ac87e3e05e7a2efd985e0695
zhihuaiwen <zhihuaiwen@tencent.com>
2022-04-15 19:56:23 +0800

    update

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mvn clean -DskipTests=true assembly:assembly
 
-docker build . -t mirrors.tencent.com/wzh-test/spark-java-k8s-demo:latest
+docker build . -t zepherywen/spark-java-k8s-demo:latest
 
-docker push mirrors.tencent.com/wzh-test/spark-java-k8s-demo:latest
+docker push zepherywen/spark-java-k8s-demo:latest
